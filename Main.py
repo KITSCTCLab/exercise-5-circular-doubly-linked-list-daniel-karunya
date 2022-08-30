3e75c7ed84977caead80f52117555d3415441bfb
daniel-karunya <91770270+daniel-karunya@users.noreply.github.com>
2022-08-30 15:28:57 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -105,7 +105,7 @@ class DoublyCircularLinkedList:
     def getall(self) -> int:
 
         n = self.head
-        for(int x in range (0,self.size)):
+        for( z in range (0,self.size):
             print (n)
             n = n.next
 
